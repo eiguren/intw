9ac8841dce8652e5e1782adb92729bf138ecdda4
jlbartolome <jon.lafuente@ehu.eus>
2023-07-07 15:58:42 +0200

    Started to clean dependence on num_bands from W90
    Still doesn't work, some tests don't pass.
    
    I think I found an inconsistency.
    In allocate_and_get_all_irreducible_wfc, wfc_k_irr_all is allocated and filled with nbands, i.e. number of bands used in QE.
    In get_psi_general_k_all_wfc, wfc_k and wfc_k_irr are filled with num_bands, i.e. number of bands used for wannierization.
    These might differ if exclude_bands is used. In any case nbands>num_bands and that's why I guess we don't get segmentation fault.
    I think this is artifically fixed in reading.f90 -> get_K_folder_data.
    Here "w90_parameters: num_exclude_bands" is used to only read the bands to be used in wannierization from the QE folders.
    But I think this is confusing, since for this to work read_nnkp_file from intw2wannier.f90 has to be called before,
    where num_exclude_bands and exclude_bands(:) is read from the .nnkp file and passed to the w90 variables.
    We should make this clearer; there's three set of bands:
    (i) nbands (computed with QE)
    (ii) num_bands (bands used for wannierization, where some might have been excluded)
    (iii) num_wann (number of Wannier functions, which might be smaller than num_bands if disentanglement is used)
    My first thought is that allocate_and_get_all_irreducible_wfc read all the nbands
    and then get_psi_general_k_all_wfc should only store and rotate the relevant bands:
    if wannier is used num_bands and if is not nbands.
    This could be done by adding the number of bands to be read/stored/rotated as input.
    Discuss with Asier, Maria and Haritz.

--- src/modules/all_wfcs.f90
+++ src/modules/all_wfcs.f90
@@ -1,3 +1,16 @@
+!! JLB: I think I found an inconsistency here. 
+!!      In allocate_and_get_all_irreducible_wfc, wfc_k_irr_all is allocated and filled with nbands, i.e. number of bands used in QE.
+!!      In get_psi_general_k_all_wfc, wfc_k and wfc_k_irr are filled with num_bands, i.e. number of bands used for wannierization.
+!!      These might differ if exclude_bands is used. In any case nbands>num_bands and that's why I guess we don't get segmentation fault.
+!!      I think this is "artifically" fixed in reading.f90 -> get_K_folder_data.
+!!      Here w90_parameters: num_exclude_bands is used to only read the bands to be used in wannierization from the QE folders.
+!!      But I think this is confusing, since read_nnkp_file from intw2wannier.f90 has to be called before,
+!!      where num_exclude_bands and exclude_bands(:) is read from the .nnkp file and passed to the w90 variables.
+!!      We should make this clearer. Three set of bands: 
+!!      (i) nbands (computed with QE) 
+!!      (ii) num_bands (bands used for wannierization, where some might have been excluded)
+!!      (iii) num_wann (number of Wannier functions, which might be smaller than num_bands if disentanglement is used)
+
 module intw_allwfcs
   ! 1. Modulo honetan uhin funtzioak irakurri transformatu eta berriro inv-transformatuko ditugu igk, igkq lista
   ! berdinak erabiliz. Era horretan sinplifikazio nabarmenta lortuko dugu.
@@ -27,7 +40,7 @@ contains
 
     use intw_reading, only: nG_max, nkpoints_QE, get_K_folder_data_with_nG, nspin, nbands,get_K_folder_data
     use intw_useful_constants, only: cmplx_0
-    use w90_parameters, only: num_wann, num_bands
+    !use w90_parameters, only: num_wann, num_bands
 
     implicit none
 
