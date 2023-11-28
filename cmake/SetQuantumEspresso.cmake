6a4996931027bb05eca7e88b58a976945ea9c655
Haritz Garai <hgaraim@gmail.com>
2023-11-28 19:03:22 +0100

    build: Check if QE and Wannier are configured and if not, disable related tests.

--- cmake/SetQuantumEspresso.cmake
+++ cmake/SetQuantumEspresso.cmake
@@ -2,65 +2,74 @@
 #
 #############################################################################
 
-message("-- Checking QE.")
-
 # List of supported QE versions
 set(SUPPORTED_QE
   "6.7+"
   )
 
+if (NOT QE_VERSION)
 
-# Find QE build dir
-file(GLOB files_and_dirs RELATIVE ${QE_HOME} ${QE_HOME}/*)
-set(dirs "")
-foreach(file_or_dir ${files_and_dirs})
-  if(IS_DIRECTORY ${QE_HOME}/${file_or_dir})
-    list(APPEND dirs ${file_or_dir})
+  message("-- Checking QE.")
+
+
+  # Find QE build dir
+  file(GLOB files_and_dirs RELATIVE ${QE_HOME} ${QE_HOME}/*)
+  set(dirs "")
+  foreach(file_or_dir ${files_and_dirs})
+    if(IS_DIRECTORY ${QE_HOME}/${file_or_dir})
+      list(APPEND dirs ${file_or_dir})
+    endif()
+  endforeach()
+
+  find_file(QE_CMAKE CMakeCache.txt PATHS ${QE_HOME} PATH_SUFFIXES ${dirs} NO_DEFAULT_PATH)
+  set(QE_CMAKE "${QE_CMAKE}" CACHE INTERNAL "Quantum Espresso cmake cache.")
+  if("${QE_CMAKE}" STREQUAL "QE_CMAKE-NOTFOUND")
+    message("-- QE built with autotools.")
+    set(QE_BUILD_DIR ${QE_HOME})
+  else()
+    message("-- QE built with CMake.")
+    get_filename_component(QE_BUILD_DIR ${QE_CMAKE} DIRECTORY)
   endif()
-endforeach()
+  message("-- Found QE_BUILD_DIR: ${QE_BUILD_DIR}")
 
-find_file(QE_CMAKE CMakeCache.txt PATHS ${QE_HOME} PATH_SUFFIXES ${dirs} NO_DEFAULT_PATH)
-if("${QE_CMAKE}" STREQUAL "QE_CMAKE-NOTFOUND")
-  message("-- QE built with autotools.")
-  set(QE_BUILD_DIR ${QE_HOME})
-else()
-  message("-- QE built with CMake.")
-  get_filename_component(QE_BUILD_DIR ${QE_CMAKE} DIRECTORY)
+
+  # Some checks
+  message("-- Checking QE build.")
+  find_program(QE_PW_EXE pw.x PATHS ${QE_BUILD_DIR}/bin NO_DEFAULT_PATH)
+  set(QE_PW_EXE "${QE_PW_EXE}" CACHE PATH "Quantum Espresso PW executable.")
+  if("${QE_PW_EXE}" STREQUAL "QE_PW_EXE-NOTFOUND")
+    message(FATAL_ERROR "QE_PW_EXE NOT FOUND! QE is not compiled or QE_HOME is incorrect.")
+  endif()
+  find_program(QE_PH_EXE ph.x PATHS ${QE_BUILD_DIR}/bin NO_DEFAULT_PATH)
+  set(QE_PH_EXE "${QE_PH_EXE}" CACHE PATH "Quantum Espresso PH executable.")
+  if("${QE_PH_EXE}" STREQUAL "QE_PH_EXE-NOTFOUND")
+    message(FATAL_ERROR "QE_PH_EXE NOT FOUND! QE is not compiled or QE_HOME is incorrect.")
+  endif()
+
+
+  # Find QE version number
+  find_file(QE_VERSION_FILE qe_version.h PATHS ${QE_HOME}/include NO_DEFAULT_PATH)
+  set(QE_VERSION_FILE "${QE_VERSION_FILE}" CACHE INTERNAL "Quantum Espresso version file.")
+  if("${QE_VERSION_FILE}" STREQUAL "QE_VERSION_FILE-NOTFOUND")
+    # 6.6 version have the version number in a different file
+    find_file(QE_VERSION_FILE version.h PATHS ${QE_HOME}/include NO_DEFAULT_PATH)
+  endif()
+  if("${QE_VERSION_FILE}" STREQUAL "QE_VERSION_FILE-NOTFOUND")
+    # Older versions have the version number in a different file
+    find_file(QE_VERSION_FILE version.f90 PATHS ${QE_HOME}/Modules NO_DEFAULT_PATH)
+  endif()
+
+  if("${QE_VERSION_FILE}" STREQUAL "QE_VERSION_FILE-NOTFOUND")
+    message(FATAL_ERROR "QE_VERSION_FILE NOT FOUND!")
+  endif()
+
+  file(STRINGS ${QE_VERSION_FILE} QE_VERSION_LINE REGEX "version_number")
+  string(REGEX MATCH "'([0-9]+)(\\.[0-9]+)?(\\.[0-9]+)?(MaX)?(\\+)?'" QE_VERSION ${QE_VERSION_LINE})
+  string(REPLACE "'" "" QE_VERSION "${QE_VERSION}")
+  set(QE_VERSION "${QE_VERSION}" CACHE INTERNAL "Quantum Espresso version.")
+  message("-- Found QE_VERSION: ${QE_VERSION}")
+
 endif()
-message("-- Found QE_BUILD_DIR: ${QE_BUILD_DIR}")
-
-
-# Some checks
-message("-- Checking QE build.")
-find_program(QE_PW_EXE pw.x PATHS ${QE_BUILD_DIR}/bin NO_DEFAULT_PATH)
-if("${QE_PW_EXE}" STREQUAL "QE_PW_EXE-NOTFOUND")
-  message(FATAL_ERROR "QE_PW_EXE NOT FOUND! QE is not compiled or QE_HOME is incorrect.")
-endif()
-find_program(QE_PH_EXE ph.x PATHS ${QE_BUILD_DIR}/bin NO_DEFAULT_PATH)
-if("${QE_PH_EXE}" STREQUAL "QE_PH_EXE-NOTFOUND")
-  message(FATAL_ERROR "QE_PH_EXE NOT FOUND! QE is not compiled or QE_HOME is incorrect.")
-endif()
-
-
-# Find QE version number
-find_file(QE_VERSION_FILE qe_version.h PATHS ${QE_HOME}/include NO_DEFAULT_PATH)
-if("${QE_VERSION_FILE}" STREQUAL "QE_VERSION_FILE-NOTFOUND")
-  # 6.6 version have the version number in a different file
-  find_file(QE_VERSION_FILE version.h PATHS ${QE_HOME}/include NO_DEFAULT_PATH)
-endif()
-if("${QE_VERSION_FILE}" STREQUAL "QE_VERSION_FILE-NOTFOUND")
-  # Older versions have the version number in a different file
-  find_file(QE_VERSION_FILE version.f90 PATHS ${QE_HOME}/Modules NO_DEFAULT_PATH)
-endif()
-
-if("${QE_VERSION_FILE}" STREQUAL "QE_VERSION_FILE-NOTFOUND")
-  message(FATAL_ERROR "QE_VERSION_FILE NOT FOUND!")
-endif()
-
-file(STRINGS ${QE_VERSION_FILE} QE_VERSION_LINE REGEX "version_number")
-string(REGEX MATCH "'([0-9]+)(\\.[0-9]+)?(\\.[0-9]+)?(MaX)?(\\+)?'" QE_VERSION ${QE_VERSION_LINE})
-string(REPLACE "'" "" QE_VERSION "${QE_VERSION}")
-message("-- Found QE_VERSION: ${QE_VERSION}")
 
 
 # Check if the given QE version is supported by intw
@@ -70,4 +79,3 @@ if("${QE_SUPPORTED}" EQUAL "-1")
   message("Supported QE versions: ${SUPPORTED_QE}")
   message(FATAL_ERROR)
 endif()
-
--- cmake/SetWannier.cmake
+++ cmake/SetWannier.cmake
@@ -2,4 +2,41 @@
 #
 #############################################################################
 
-set(W_BUILD_DIR "${W_HOME}")
+# List of supported Wannier90 versions
+set(SUPPORTED_W
+  "3.1.0"
+  )
+
+if (NOT W_VERSION)
+
+  message("-- Checking Wannier90.")
+
+
+  # Find Wannier90 build dir
+  set(W_BUILD_DIR "${W_HOME}")
+
+
+  # Some checks
+  message("-- Checking Wannier90 build.")
+  find_program(W_EXE wannier90.x PATHS ${W_BUILD_DIR} NO_DEFAULT_PATH)
+  set(W_EXE "${W_EXE}" CACHE PATH "Wannier90 executable.")
+  if("${W_EXE}" STREQUAL "W_EXE-NOTFOUND")
+  message(FATAL_ERROR "W_EXE NOT FOUND! Wannier90 is not compiled or WANNIER_HOME is incorrect.")
+  endif()
+
+
+  # Find Wannier90 version number
+  find_file(W_VERSION_FILE io.F90 PATHS ${W_HOME}/src NO_DEFAULT_PATH)
+  set(W_VERSION_FILE "${W_VERSION_FILE}" CACHE INTERNAL "Wannier90 version file.")
+  if("${W_VERSION_FILE}" STREQUAL "W_VERSION_FILE-NOTFOUND")
+     message(FATAL_ERROR "W_VERSION_FILE NOT FOUND!")
+  endif()
+
+  file(STRINGS ${W_VERSION_FILE} W_VERSION_LINE REGEX "w90_version *=")
+  string(REGEX MATCH "' *([0-9]+)(\\.[0-9]+)?(\\.[0-9]+)? *'" W_VERSION ${W_VERSION_LINE})
+  string(REPLACE "'" "" W_VERSION "${W_VERSION}")
+  string(REPLACE " " "" W_VERSION "${W_VERSION}")
+  set(W_VERSION "${W_VERSION}" CACHE INTERNAL "Wannier90 version.")
+  message("-- Found W_VERSION: ${W_VERSION}")
+
+endif()
