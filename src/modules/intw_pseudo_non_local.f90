94bf40c420b6286cd5f5ac6dcd6a10d2851e7c63
Haritz Garai <hgaraim@gmail.com>
2024-03-22 13:49:12 +0100

    refactor(intw_pseudo_non_local): Move sph_ind and spinor subroutines to intw_pseudo_non_local module.

--- src/modules/intw_pseudo_non_local.f90
+++ src/modules/intw_pseudo_non_local.f90
@@ -207,9 +207,6 @@ contains
     complex(kind=dp) :: coeff
     real(kind=dp) :: ji, jk
 
-    real(kind=dp), external :: spinor
-    integer, external :: sph_ind
-
 
     !
     ! Initialization of the variables
@@ -763,5 +760,89 @@ contains
     end do !iband
 
   end subroutine multiply_psi_by_dvKB
+
+
+  function sph_ind(l, j, m, spin)
+    ! This function calculates the m index of the spherical harmonic
+    ! in a spinor with orbital angular momentum l, total angular
+    ! momentum j, projection along z of the total angular momentum m+-1/2.
+    ! Spin selects the up (spin=1) or down (spin=2) coefficient.
+    !
+    ! Adapted from Quantum Espresso
+
+    use kinds, only: dp
+    use intw_utility, ONLY : errore
+
+    implicit none
+
+    integer, intent(in)       :: l, & ! orbital angular momentum
+                                 m, & ! projection of the total angular momentum+-1/2
+                                 spin ! 1 or 2 select the component
+    real(kind=dp), intent(in) :: j    ! total angular momentum
+    integer :: sph_ind
+
+    if (spin.ne.1.and.spin.ne.2) call errore('sph_ind','spin direction unknown',1)
+    if (m.lt.-l-1.or.m.gt.l) call errore('sph_ind','m not allowed',1)
+
+    if (abs(j-l-0.5d0).lt.1.d-8) then
+      if (spin.eq.1) sph_ind= m
+      if (spin.eq.2) sph_ind= m+1
+    elseif (abs(j-l+0.5d0).lt.1.d-8) then
+      if (m.lt.-l+1) then
+          sph_ind=0
+      else
+          if (spin.eq.1) sph_ind= m-1
+          if (spin.eq.2) sph_ind= m
+      endif
+    else
+      write(6,*) l, j
+      call errore('sph_ind','l and j not compatible',1)
+    endif
+    if (sph_ind.lt.-l.or.sph_ind.gt.l) sph_ind=0
+
+    return
+  end function sph_ind
+
+  function spinor(l, j, m, spin)
+    ! This function calculates the numerical coefficient of a spinor
+    ! with orbital angular momentum l, total angular momentum j,
+    ! projection along z of the total angular momentum m+-1/2. Spin selects
+    ! the up (spin=1) or down (spin=2) coefficient.
+    !
+    ! Adapted from Quantum Espresso
+
+    use kinds, only: dp
+    use intw_utility, ONLY : errore
+
+    implicit none
+
+    integer, intent(in)       :: l, & ! orbital angular momentum
+                                 m, & ! projection of the total angular momentum+-1/2
+                                 spin ! 1 or 2 select the component
+    real(kind=dp), intent(in) :: j    ! total angular momentum
+    real(kind=dp) :: spinor
+
+    real(kind=dp) :: denom ! denominator
+
+    if (spin.ne.1.and.spin.ne.2) call errore('spinor','spin direction unknown',1)
+    if (m.lt.-l-1.or.m.gt.l) call errore('spinor','m not allowed',1)
+
+    denom=1.d0/(2.d0*l+1.d0)
+    if (abs(j-l-0.5d0).lt.1.d-8) then
+      if (spin.eq.1) spinor= sqrt((l+m+1.d0)*denom)
+      if (spin.eq.2) spinor= sqrt((l-m)*denom)
+    elseif (abs(j-l+0.5d0).lt.1.d-8) then
+      if (m.lt.-l+1) then
+          spinor=0.d0
+      else
+          if (spin.eq.1) spinor= sqrt((l-m+1.d0)*denom)
+          if (spin.eq.2) spinor= -sqrt((l+m)*denom)
+      endif
+    else
+      call errore('spinor','j and l not compatible',1)
+    endif
+
+    return
+  end function spinor
 
 end module intw_pseudo_non_local
