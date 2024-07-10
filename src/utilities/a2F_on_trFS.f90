dea884c05be6f44b5eff7cb7a53fb201e022c6d0
mblancorey <mblanrey78@gmail.com>
2024-07-10 16:55:43 +0200

    Calculate a2F from a file of interpolated ep on FS. Gives right DOS at E_f and ph-DOS, but still something wrong with the obtained a2F

--- src/utilities/a2F_on_trFS.f90
+++ src/utilities/a2F_on_trFS.f90
@@ -12,14 +12,15 @@
 
 program a2F_on_trFS
 
-        use kinds, only: dp
-        use intw_useful_constants, only: cmplx_1, cmplx_0, cmplx_i, Ha_to_eV, tpi, eps_8
-        use intw_utility, only: find_free_unit, cryst_to_cart, area_vec, smeared_delta, &
+        use kinds, only: dp    
+        use intw_useful_constants, only: cmplx_1, cmplx_0, cmplx_i, Ha_to_eV, tpi, eps_8, eps_6
+        use intw_utility, only: find_free_unit, cryst_to_cart, area_vec, smeared_delta, smeared_lorentz, &
                 generate_kmesh
 
         use intw_input_parameters, only: mesh_dir, prefix, read_input,&
-                nk1, nk2, nk3, nq1, nq2, nq3, nqirr, &
+                nk1, nk2, nk3, nq1, nq2, nq3, nqirr, ph_dir, fc_mat, &
                 nomega, omega_ini, omega_fin, osmear_q, &
+                read_for_dynmat, &
                 ep_interp_bands, nfs_sheets_initial, nfs_sheets_final
         use intw_reading, only: read_parameters_data_file_xml, set_num_bands, &
                 nspin, at, bg, volume0, alat, nat, ntyp, ityp, amass, &
@@ -27,8 +28,9 @@ program a2F_on_trFS
         use intw_ph, only: nqmesh, qmesh, frc, readfc, read_ph_information_xml, &
                 q_irr, q_irr_cryst
 
-        use intw_ph_interpolate, only: allocate_and_build_dyn_qmesh2,  dyn_q_to_dyn_r, &
-                allocate_and_build_ws_irvec_q, dyn_interp_1q, dyn_diagonalize_1q, &
+        use intw_ph_interpolate, only: allocate_and_build_dyn_qmesh, &
+                allocate_and_build_dyn_qmesh2, dyn_diagonalize_1q, &
+                allocate_and_build_ws_irvec_qtau, dyn_interp_1q_tau, dyn_q_to_dyn_rtau, &
                 w2_q, u_q
 
     implicit none
@@ -42,12 +44,14 @@ program a2F_on_trFS
         integer :: nkpt_tr_tot, nkpt_tr_ibz_tot
         integer :: is, js, ikpt, ik, ik1, i,j,k, iface, ir, ir1, ir2, ir3
         integer  :: nfs_sheets_tot ! number of sheets considered
-        integer , allocatable :: nfs_sheet(:), & ! band indices of the sheets (num_bands_intw set)
-                                 nkpt_tr(:), &  ! number of kpoints in each FS sheet
+        integer , allocatable :: nfs_sheet(:), &    ! band indices of the sheets (num_bands_intw set)
+                                 nkpt_tr(:), &      ! number of kpoints in each FS sheet
                                  nkpt_tr_ibz(:), &  ! number of kpoints in each FS sheet irreducible BZ wedge
-                                 nface_tr(:)  ! number of faces in each FS sheet
+                                 nface_tr(:), &     ! number of faces in each FS sheet
+                                 nface_tr_ibz(:)    ! number of faces in each IFS 
         real(dp) :: k1(3), k2(3), k3(3), kwei, vol1bz
-        real(dp), allocatable :: kpts_tr(:,:), kpts_tr_area(:), vk_tr(:,:), vabsk_tr(:)
+        real(dp), allocatable :: kpts_tr(:,:), kpts_tr_area(:), vk_tr(:,:), vabsk_tr(:), &
+                                 kpts_tr_ibz(:,:), kpts_tr_ibz_area(:), vk_tr_ibz(:,:), vabsk_tr_ibz(:)
         real(dp) , allocatable :: area_ibz(:), area_fbz(:), factor_area_ibz(:)
 
         logical                  :: have_ep
@@ -85,7 +89,7 @@ write(*,'(A)') '|    waiting for input file...                      |'
 
 !================================================================================
 !       read the input file
-!       Read in the necessary information from standard input
+!       Read in the necessary information from standard input 
 !       (for this utility, only prefix needed, really)
 !================================================================================
 
@@ -122,7 +126,7 @@ write(*,'(A)') '====================================================='
 write(*,'(A)') '|    reading .off and v_k files...                  |'
 
 allocate (nkpt_tr(nfs_sheets_tot), nface_tr(nfs_sheets_tot))
-allocate (nkpt_tr_ibz(nfs_sheets_tot))
+allocate (nkpt_tr_ibz(nfs_sheets_tot), nface_tr_ibz(nfs_sheets_tot))
 
 !open all sheet files just to see dimensions of kpoint lists
 unit_off=find_free_unit()
@@ -132,21 +136,22 @@ do is=1,nfs_sheets_tot
    if (                is <   10) write(is_loc,"(i1)") nfs_sheet(is)
    if ( 10 <= is .and. is <  100) write(is_loc,"(i2)") nfs_sheet(is)
 
-   file_off=trim(mesh_dir)//trim(prefix)//'.'//trim(adjustl(is_loc))//'_FS_tri.off'
+   file_off=trim(mesh_dir)//trim(prefix)//trim('.')//trim(adjustl(is_loc))//trim('_FS_tri.off')
+   write(*,*) is, file_off
 
    open(unit_off, file=file_off, status='old')
    read(unit_off,*) comenta
-   read(unit_off,*) nkpt_tr(is), nface_tr(is)  !number of vertices and faces (ignore edges)
+   read(unit_off,*) nkpt_tr(is), nface_tr(is)  !number of vertices and faces (ignore edges) 
    close(unit_off)
 
-   !open the IBZ off file and search for dimension nkpt_tr_ibz(is).
+   !open the IBZ off file and search for dimension nkpt_tr_ibz(is). 
    !Its vertices coincide with the first nkpt_tr_ibz(is) vertices of the full off vertex list.
 
-   file_off=trim(mesh_dir)//trim(prefix)//'.'//trim(adjustl(is_loc))//'_newton_IBZ_FS_tri.off'
+   file_off=trim(mesh_dir)//trim(prefix)//trim('.')//trim(adjustl(is_loc))//trim('_newton_IBZ_FS_tri.off')
 
    open(unit_off, file=file_off, status='old')
    read(unit_off,*) comenta
-   read(unit_off,*) nkpt_tr_ibz(is)  !number of vertices (ignore rest)
+   read(unit_off,*) nkpt_tr_ibz(is),  nface_tr_ibz(is)  !number of vertices and faces (ignore rest)
    close(unit_off)
 
 end do
@@ -155,8 +160,6 @@ end do
 nkpt_tr_tot=sum(nkpt_tr)
 nkpt_tr_ibz_tot=sum(nkpt_tr_ibz)
 
-write(*,'(A)') '|   .off file                                       |'
-write(*,*) is, file_off
 write(*,'(A)') '|   Number of k-points (total vertices):            |'
 write(*,*)  nkpt_tr_tot
 write(*,'(A)') '|   Number of k-points in IBZ (total vertices):     |'
@@ -164,8 +167,9 @@ write(*,*)  nkpt_tr_ibz_tot
 write(*,'(A)') '|   Number of total faces:                          |'
 write(*,*) sum(nface_tr)
 
-
+allocate(kpts_tr_ibz(3,nkpt_tr_ibz_tot), kpts_tr_ibz_area(nkpt_tr_ibz_tot))
 allocate(kpts_tr(3,nkpt_tr_tot), kpts_tr_area(nkpt_tr_tot))
+allocate(vk_tr_ibz(3,nkpt_tr_ibz_tot), vabsk_tr_ibz(nkpt_tr_ibz_tot) )
 allocate(vk_tr(3,nkpt_tr_tot), vabsk_tr(nkpt_tr_tot) )
 
 !open .off's again, read k-points and read velocity files
@@ -177,26 +181,26 @@ do is=1,nfs_sheets_tot
 
    ! .off file for this sheet
 
-   file_off=trim(mesh_dir)//trim(prefix)//'.'//trim(adjustl(is_loc))//'_FS_tri.off'
+   file_off=trim(mesh_dir)//trim(prefix)//trim('.')//trim(adjustl(is_loc))//trim('_FS_tri.off') 
    open(unit_off, file=file_off, status='old')
 
    read(unit_off,*) comenta
    read(unit_off,*) i,j,k  !number vertices, faces and edges (I will ignore edges)
    !read(unit_off,'(/)') !DUDA... esto dependdera de como está escrito el salto de línea en el fichero, creo...
-   if  ( i .ne. nkpt_tr(is) .or. j .ne. nface_tr(is) ) then
+   if  ( i .ne. nkpt_tr(is) .or. j .ne. nface_tr(is) ) then 
            write(*,*)'Error reading ', file_off, '. Stopping.'
            stop
    end if
 
    !read vertices
-   do ik=1,nkpt_tr(is)
+   do ik=1,nkpt_tr(is) 
       read(unit_off,*) kpts_tr(:,ik1+ik)  !units in the trFS.off file are cartesian 2pi/alat ("tpiba" for QE)
    end do
 
    !Read (triangular) faces on this sheet.
    !Each face contributes with 1/3 of its area to the effective area of each of its vertices.
    !Calculate the are on the go and add the contribution to each vertex, storing for global indices (i.e. ik1+ik).
-   do iface = 1,nface_tr(is)
+   do iface = 1,nface_tr(is) 
       read(unit_off,*) i, ir1,ir2,ir3 !indices ik of the vertices of the face, indexed from 0
       ir1=ir1+1
       ir2=ir2+1
@@ -218,9 +222,10 @@ do is=1,nfs_sheets_tot
    end do
 
    close(unit_off)
+
    ! velocity for this sheet (use same unit)
 
-   file_off=trim(mesh_dir)//trim(prefix)//'.'//trim(adjustl(is_loc))//'_FS_v_k.dat'
+   file_off=trim(mesh_dir)//trim(prefix)//trim('.')//trim(adjustl(is_loc))//trim('_FS_v_k.dat')
    open(unit_off, file=file_off, status='old')
 
    read(unit_off,*) i
@@ -229,14 +234,89 @@ do is=1,nfs_sheets_tot
            stop
    end if
 
-   do ik=1,nkpt_tr(is)
+   do ik=1,nkpt_tr(is) 
       read(unit_off,*) i, vk_tr(:,ik1+ik), vabsk_tr(ik1+ik)  !velocity xyz and its modulus. DUDA 2pi/alat???
    end do
 
    close(unit_off)
 
    !accumulate ik global index for the reading of next sheet
-   ik1=ik1+nkpt_tr(is)
+   ik1=ik1+nkpt_tr(is) 
+
+end do
+
+
+! MBR correction 100724
+!now do the same with irreducible BZ off's to obtain the triangle areas there
+ik1 = 0
+do is=1,nfs_sheets_tot
+
+   if (                is <   10) write(is_loc,"(i1)") nfs_sheet(is)
+   if ( 10 <= is .and. is <  100) write(is_loc,"(i2)") nfs_sheet(is)
+
+   ! .off file for this sheet
+
+   file_off=trim(mesh_dir)//trim(prefix)//trim('.')//trim(adjustl(is_loc))//trim('_newton_IBZ_FS_tri.off')
+   open(unit_off, file=file_off, status='old')
+
+   read(unit_off,*) comenta
+   read(unit_off,*) i,j,k  !number vertices, faces and edges (I will ignore edges)
+   !read(unit_off,'(/)') !DUDA... esto dependdera de como está escrito el salto de línea en el fichero, creo...
+   if  ( i .ne. nkpt_tr_ibz(is) .or. j .ne. nface_tr_ibz(is)  ) then
+           write(*,*)'Error reading ', file_off, '. Stopping.'
+           stop
+   end if
+
+   !read vertices (actually, these are repeated from the first nkpt_tr_ibz(is) lines of the full BZ file)
+   do ik=1,nkpt_tr_ibz(is)
+      read(unit_off,*) kpts_tr_ibz(:,ik1+ik)  !units in the trFS.off file are cartesian 2pi/alat ("tpiba" for QE)
+   end do
+
+   !Read (triangular) faces on this sheet.
+   !Each face contributes with 1/3 of its area to the effective area of each of its vertices.
+   !Calculate the are on the go and add the contribution to each vertex, storing for global indices (i.e. ik1+ik).
+   do iface = 1,nface_tr_ibz(is)
+      read(unit_off,*) i, ir1,ir2,ir3 !indices ik of the vertices of the face, indexed from 0
+      ir1=ir1+1
+      ir2=ir2+1
+      ir3=ir3+1  !now, ik of the vertices of the face, indexed from 1
+      if  ( i .ne. 3 ) then
+           write(*,*)'Error reading ', file_off, 'Only triangles allowed. Stopping.'
+           stop
+      end if
+      !triangle vertex vectors (cartesian 2pi/alat)
+      k1 = kpts_tr_ibz(:,ik1+ir1)
+      k2 = kpts_tr_ibz(:,ik1+ir2)
+      k3 = kpts_tr_ibz(:,ik1+ir3)
+      !get spanned area and add contribution to each vertex
+      !function copied from FSH/modules/geometry.f90
+      kwei = area_vec(k2-k1,k3-k1)/3.0_dp
+      kpts_tr_ibz_area(ik1+ir1) = kpts_tr_ibz_area(ik1+ir1) + kwei
+      kpts_tr_ibz_area(ik1+ir2) = kpts_tr_ibz_area(ik1+ir2) + kwei
+      kpts_tr_ibz_area(ik1+ir3) = kpts_tr_ibz_area(ik1+ir3) + kwei
+   end do
+
+   close(unit_off)
+
+  ! velocity for this sheet (use same unit)
+
+   file_off=trim(mesh_dir)//trim(prefix)//trim('.')//trim(adjustl(is_loc))//trim('_IBZ_v_k.dat')
+   open(unit_off, file=file_off, status='old')
+
+   read(unit_off,*) i
+   if  ( i .ne. nkpt_tr_ibz(is) ) then
+           write(*,*)'Error reading ', file_off, '. Stopping.'
+           stop
+   end if
+
+   do ik=1,nkpt_tr_ibz(is)
+      read(unit_off,*) i, vk_tr_ibz(:,ik1+ik), vabsk_tr_ibz(ik1+ik)  !velocity xyz and its modulus. DUDA 2pi/alat???
+   end do
+
+   close(unit_off)
+
+   !accumulate ik global index for the reading of next sheet
+   ik1=ik1+nkpt_tr_ibz(is)
 
 end do
 
@@ -261,7 +341,7 @@ end do
   do ish = 1, nfs_sheets_tot
   do iks = 1, nkpt_tr_ibz(ish)
       ik = ik + 1
-      area_ibz(ish) = area_ibz(ish) + kpts_tr_area(ik)
+      area_ibz(ish) = area_ibz(ish) + kpts_tr_ibz_area(ik)
   end do
   end do
   ik = 0
@@ -274,6 +354,8 @@ end do
   do ish=1,nfs_sheets_tot
      factor_area_ibz(ish) = area_fbz(ish)/area_ibz(ish)
   end do
+  write(*,*) 'area in full 1BZ (tpiba^2) = ', area_fbz(:)
+  write(*,*) 'area in irr  1BZ (tpiba^2) = ', area_ibz(:)
   write(*,*) 'factor_area_ibz = ', factor_area_ibz(:)
 
 
@@ -282,7 +364,7 @@ end do
 !================================================================================
 
   unit_ep=find_free_unit()
-  file_ep=trim(mesh_dir)//trim(prefix)//'_ep_interp.dat'
+  file_ep=trim(mesh_dir)//trim(prefix)//trim('_ep_interp.dat')
   inquire(file=file_ep,exist=have_ep)
 
   if (.not.have_ep) then
@@ -293,7 +375,7 @@ end do
      write(*,*) '!      Stopping                                               !'
      stop
 
-  else
+  else       
 
     ! Note, indices ikp,ik include all the calculated sheets
     allocate(aep_mat_el(nkpt_tr_tot,nkpt_tr_ibz_tot,nspin,nspin,3*nat))
@@ -305,19 +387,20 @@ end do
     do j = 1,nkpt_tr_ibz_tot*nspin
         do js=1,nspin
         do is=1,nspin
-           read(unit_ep,fmt="(2i6,3f10.4,100e16.6)") ikp,ik, kpoint, &
+!           read(unit_ep,fmt="(2i6,3f10.4,100e16.6)") ikp,ik, kpoint, &
+            read(unit_ep,fmt="(6i6,100e16.6)") ibp, iksp, ikp, ib, iks, ik,&
                  (aep_mat_el(ikp,ik, js,is,iat), iat=1,3*nat)
         end do
-        end do
+        end do 
     end do
-    end do
+    end do        
 
     close(unit_ep)
 
     write(*,*) '!     interpolated e-p elements read from file:               !'
-    write(*,*) file_ep
+    write(*,*) file_ep         
 
-  end if
+  end if  
 
 !================================================================================
 ! Read all the information about phonons and prepare for interpolation of phonons:
@@ -330,21 +413,22 @@ end do
   call generate_kmesh(qmesh,nq1,nq2,nq3)
   ! This Wigner-Seitz mesh will be needed to interpolate w_q below
   ! (q->R space will be done on that grid of nrpts_q lattice points).
-  call allocate_and_build_ws_irvec_q()
+  call allocate_and_build_ws_irvec_qtau()
 
   ! q_irr vectors info is obtained through this (it is contained in qlist.txt file)
   call read_ph_information_xml()
 
-  ! INTW has two options to get dyn_q:
-  ! 1. read force constants
-  !  fc_file_name = trim(mesh_dir)//trim(ph_dir)//trim(fc_mat)
-  !  call allocate_and_build_dyn_qmesh(fc_file_name)
-  ! or:
-  ! 2. read dyn files directly (new by MBR, not fully tested)
-  call allocate_and_build_dyn_qmesh2()
+
+  print *,' get dyn_q matrices'
+  ! Two options to get dyn_q:
+  if (read_for_dynmat == 'fc' ) then ! read force constants
+          call allocate_and_build_dyn_qmesh(fc_mat)
+  else if (read_for_dynmat == 'dynq' ) then ! read dyn files
+          call allocate_and_build_dyn_qmesh2()
+  end if
 
   ! transform to R space
-  call dyn_q_to_dyn_r()
+  call dyn_q_to_dyn_rtau()
 
   ! Now w2_q contains phonon frequencies and u_q contains the polarization vectors.
   ! Phonon frequencies (negative means unstable mode)
@@ -376,7 +460,7 @@ end do
 
   ! output file for a2F
   unit_a2f=find_free_unit()
-  file_a2f=trim(mesh_dir)//trim(prefix)//'_a2F_interp.dat'
+  file_a2f=trim(mesh_dir)//trim(prefix)//trim('_a2F_interp.dat')
   open(unit_a2f, file=file_a2f, status='unknown')
   write(unit_a2f,'(A)') '#omega(Ry)  alpha2F(total)    alpha2F(1:nmode)'
 
@@ -389,12 +473,14 @@ end do
 ! to be selected, so instead of iterating over nkpt_tr_tot, I separate over sheets
 
 ik = 0
+ik1 = 0
 do ish = 1, nfs_sheets_tot
       ib = nfs_sheet(ish) !band index for k
 do iks = 1, nkpt_tr_ibz(ish)
-      ik = ik + 1   !k-index over nkpt_tr_tot in the Irreducible BZ
+      ! ik is the k-index over nkpt_tr_tot in the Irreducible BZ
+      ik = ik + 1
 
-      kpoint = kpts_tr(:,ik)  ! this is cartesians x 2pi/alat. Transform to cryst.
+      kpoint = kpts_tr_ibz(:,ik) ! this is cartesians x 2pi/alat. Transform to cryst.
       call cryst_to_cart (1, kpoint, at, -1)
 
   ikp = 0
@@ -409,7 +495,7 @@ do iks = 1, nkpt_tr_ibz(ish)
       qpoint = kpoint_p-kpoint
 
       !interpolate phonon:
-      call dyn_interp_1q(qpoint, dyn_qint)
+      call dyn_interp_1q_tau(qpoint, dyn_qint)
       call dyn_diagonalize_1q(3*nat, dyn_qint, u_qint, w2_qint)
       w_qint=sign(sqrt(abs(w2_qint)),w2_qint)
       !u_qint contains the polarization vector (each column is a mode)
@@ -433,22 +519,24 @@ do iks = 1, nkpt_tr_ibz(ish)
             if (w_qint(imode)>eps_8) then !stable mode frequency
                do iat=1,3*nat
                   k=((iat-1)/3)+1  !atom index
-                  rfacq=2.0_dp * w_qint(imode) * amass(ityp(k)) * pmass
+                  rfacq=2.0_dp * w_qint(imode) * amass(ityp(k)) * pmass 
                   gep_int(imode) = gep_int(imode)  +  &
-                       aep_mat_el(ikp,ik,is,js,iat) * u_qint(iat,imode) / sqrt(rfacq)
+                      aep_mat_el(ikp,ik,is,js,iat) * u_qint(iat,imode) / sqrt(rfacq)
                end do
             end if
          end do
 
         ! Sum over modes and weight with areas and velocities v_k * v_k' for contribution of k',k pair to a2F.
         ! Mind, the areas were in (tpi/alat)**2 units.
+        ! Add weight of this irreducible wedge (IBZ) to the integral using factor_area_ibz.
 
-        dsk_vk_2 = kpts_tr_area(ik) * kpts_tr_area(ikp) * (tpi/alat)**4 / (vabsk_tr(ik) * vabsk_tr(ikp) )
-        dsk_vk_2 = dsk_vk_2 * factor_area_ibz(ish) ! adds weight of this irreducible wedge (IBZ) to the integral
+        dsk_vk_2 = kpts_tr_ibz_area(ik) * factor_area_ibz(ish) * kpts_tr_area(ikp) * &
+                   (tpi/alat)**4 / (vabsk_tr_ibz(ik) * vabsk_tr(ikp) )
         do iomega = 1, nomega  !frequencies
            omega = omega_ini + omega_step*real(iomega-1,dp)
            do imode = 1,3*nat
-              rfacq = smeared_delta(omega-w_qint(imode), osmear_q)  !smear omega(q)
+              !rfacq = smeared_delta(omega-w_qint(imode), osmear_q)  !smear omega(q)
+              rfacq = smeared_lorentz(omega-w_qint(imode), osmear_q)  !smear omega(q)
               alpha2F(is,js,imode,iomega) = alpha2F(is,js,imode,iomega) + &
                       (abs(gep_int(imode)))**2 * rfacq * dsk_vk_2
            end do !imode
@@ -466,12 +554,12 @@ end do !k  (sheet)
   ! divide by N(E_F) and 1BZ volume
   ! TODO:
   ! DUDA con la normalizacion... para variar...
-  ! DUDA con el spin...
+  ! DUDA con el spin... 
   alpha2F=alpha2F/dosef/vol1bz
 
   !  print out by spin blocks
-  do is=1,nspin
-  do js=1,nspin
+  do is=1,nspin 
+  do js=1,nspin 
 
      write(unit_a2f,*) '#spins = ', is,js
 
@@ -483,7 +571,7 @@ end do !k  (sheet)
         omega = omega + omega_step
      end do
 
-     ! calculate lambda
+     ! calculate lambda 
      write(unit_a2f,*) '# lambda = 2 \int dw a2F(w)/w for each mode'
      do imode = 1,3*nat
         lambda(imode) = 0.0_dp
@@ -511,5 +599,5 @@ end do !k  (sheet)
 
     deallocate(aep_mat_el)
 
-    stop
+    stop     
 end program a2F_on_trFS
