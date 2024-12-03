23feb25eb5e8bd4110dc86b3e53aa194e0431636
Haritz Garai <haritz.garai@ehu.eus>
2024-12-03 19:08:48 +0100

    feat(siesta2intw): Use QE spline interpolation routines.

--- src/siesta2intw/4.1.5/siesta2intw_write.F90
+++ src/siesta2intw/4.1.5/siesta2intw_write.F90
@@ -1193,6 +1193,8 @@ contains
     ! variables
     use siesta_options, only: g2cut
     use atm_types, only: species, nspecies
+    ! functions and subroutines
+    use siesta2intw_utils, only: spline
 
     implicit none
 
@@ -1222,7 +1224,7 @@ contains
              phi_g_table(is,io,iq) = g_of_phi(is,io,gg_list(iq))
           enddo ! iq
           !
-          call spline_dp(gg_list, phi_g_table(is,io,:), nq, phi_g_y2(is,io,:))
+          call spline(gg_list, phi_g_table(is,io,:), 0.0_dp, 0.0_dp, phi_g_y2(is,io,:))
           !
        end do ! io
        !
@@ -1236,6 +1238,9 @@ contains
     !
     !
 
+    ! functions and subroutines
+    use siesta2intw_utils, only: splint
+
     implicit none
 
     integer, intent(in) :: is, io ! specie and orbital index
@@ -1248,7 +1253,7 @@ contains
        return
     end if
     !
-    call splint_dp(gg_list, phi_g_table(is,io,:), phi_g_y2(is,io,:), size(gg_list), q, spline_g_of_phi)
+    spline_g_of_phi = splint(gg_list, phi_g_table(is,io,:), phi_g_y2(is,io,:), q)
 
   end function spline_g_of_phi
 
@@ -1401,122 +1406,5 @@ contains
     end do!k
 
   end subroutine irreduciblek
-
-
-  SUBROUTINE SPLINE_dp(X,Y,N,Y2,YP1,YPN)
-    !
-    ! Given arrays x and y of size n, this routine computes
-    ! an array y2 holding information about the second derivative
-    ! of the interpolating spline. YP1 and YPN, if present, are the
-    ! requested values of the first derivative of the spline at the
-    ! end points. If not given, a "natural" spline with zero second
-    ! derivative at the extremes is constructed.
-    ! The array y2 is needed to evaluate the spline, but it only needs
-    ! to be computed once.
-    !
-
-    implicit none
-
-    integer, intent(in) :: n
-    real(kind=dp), dimension(:), intent(in) :: x, y
-    real(kind=dp), dimension(:), intent(out) :: y2
-    real(kind=dp), intent(in), optional :: yp1, ypn
-
-    real(kind=dp) :: P, QN, SIG, UN
-    integer :: I, K
-
-    real(kind=dp), parameter :: zero  = 0.0_dp, &
-                                half  = 0.5_dp, &
-                                one   = 1.0_dp, &
-                                two   = 2.0_dp, &
-                                three = 3.0_dp, &
-                                six   = 6.0_dp
-
-    real(kind=dp), dimension(n) :: U ! Automatic array
-
-    !--------------------------------------------------------
-    do i = 1, N-1
-       if (x(i+1) - x(i) <= 0.0_dp)  then
-          STOP " X ha de estar en orden creciente en spline"
-       end if
-    enddo
-
-    IF (present(YP1)) THEN
-       Y2(1) = -half
-       U(1) = (three/ (X(2)-X(1)))* ((Y(2)-Y(1))/ (X(2)-X(1))-YP1)
-    ELSE
-       Y2(1) = zero
-       U(1) = zero
-    END IF
-
-    DO I = 2,N - 1
-       SIG = (X(I)-X(I-1))/ (X(I+1)-X(I-1))
-       P = SIG*Y2(I-1) + two
-       Y2(I) = (SIG-one)/P
-       U(I) = ( six * ((Y(I+1)-Y(I))/ (X(I+1)-X(I))- (Y(I)-Y(I-1))/ &
-            (X(I)-X(I-1)))/ (X(I+1)-X(I-1))-SIG*U(I-1))/P
-    enddo
-    IF (present(YPN)) THEN
-       QN = half
-       UN = (three/(X(N)-X(N-1)))* (YPN - (Y(N)-Y(N-1))/ (X(N)-X(N-1)))
-    ELSE
-       QN = zero
-       UN = zero
-    END IF
-
-    Y2(N) = (UN-QN*U(N-1))/ (QN*Y2(N-1) + one)
-    DO K = N-1 , 1 , -1
-       Y2(K) = Y2(K)*Y2(K+1) + U(K)
-    enddo
-
-  end subroutine spline_dp
-
-
-  SUBROUTINE SPLINT_dp(XA, YA, Y2A, N, X, Y)
-    !
-    ! Given arrays xa and ya of size n, and an array y2a computed
-    ! previously by routine spline, this routine computes the value
-    ! at x of the interpolating spline. The value is returned in y.
-    !
-
-    ! variables
-    use siesta2intw_io, only: stdout
-
-    implicit none
-
-    integer, intent(in) :: n
-    real(kind=dp), dimension(:), intent(in) :: xa, ya, y2a
-    real(kind=dp), intent(in) :: x
-    real(kind=dp), intent(out) :: y
-
-    real(kind=dp) :: A, B, H
-    integer :: K, KHI, KLO
-
-    KLO = 1
-    KHI = N
-    do
-       IF (KHI-KLO <= 1) then
-          exit
-       end if
-       K = (KHI+KLO)/2
-       IF (XA(K) > X) THEN
-          KHI = K
-       ELSE
-          KLO = K
-       END IF
-    enddo
-
-    H = XA(KHI) - XA(KLO)
-    IF (H == 0.0_dp) THEN
-       write(stdout,*) "Bad XA input."
-       RETURN
-    END IF
-
-    A = (XA(KHI)-X)/H
-    B = (X-XA(KLO))/H
-    Y = A*YA(KLO) + B*YA(KHI) + ((A**3.0_dp-A)*Y2A(KLO)+  &
-         (B**3.0_dp-B)*Y2A(KHI))* (H**2.0_dp)/6.0_dp
-
-  end subroutine splint_dp
 
 end module siesta2intw_write
